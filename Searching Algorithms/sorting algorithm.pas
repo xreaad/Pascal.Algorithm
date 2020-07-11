7d6ef141a6dfd8793cb8c228dc6881b3fc1c3ef9
Riadh Chelbi <59140798+xreaad@users.noreply.github.com>
2020-07-11 05:59:41 -0500

    Update sorting algorithm.pas

--- Searching Algorithms/sorting algorithm.pas
+++ Searching Algorithms/sorting algorithm.pas
@@ -21,13 +21,13 @@ var
     {function take 3 arg : T type TAB => the array to do the process on
                    i var integer => the variable of where the no sorting part start
                    n var integer => taille of the array }
-        var
+     var
         {local variable => inside the function}
         {maximum var integer => the maximum value in the array (no sorting part)}
         {index var integer => the index of maximum element in the array (no sorting part)}
         {j var integer => the counter of for loop}
                    maximum, index, j : integer;
-        begin
+     begin
         {the maximum variable take the first element in the array (no sorting part)
         and start to find the bigger element in this part}
         {for loop start from the var i => i is the index of first item in (no sorting part} 
@@ -43,7 +43,7 @@ var
                                 end;
 
                 Max := index; {return the index of maximum item in the array (no sorting part)}
-        end;
+     end;
     
     procedure replace(var T:TAB; i, m:integer);
     {procedure to replace the value}
@@ -55,7 +55,7 @@ var
     end;
 
 
-        procedure Sort(var T:TAB; n:integer);
+    procedure Sort(var T:TAB; n:integer);
     {procedure Sort => to change the item position in the array to sorted it in order}
     {item var integer => to put the variable change into - it's the one way to do that !}
     {m var integer => the return value from the Max function}
@@ -80,7 +80,8 @@ begin
 
     writeln('Hi From Sort Algorithm');
     writeln();
-    write('First well will fill an array of integer, please input the length of the array :  ');        readln(n);
+    write('First well will fill an array of integer, please input the length of the array :  ');        
+    readln(n);
     
     writeln();    
     writeln('Fill the list with integer element');
@@ -97,5 +98,3 @@ begin
     writeln();
 
 end.
-
-  
